9ab758500da1fff7eacbddc338cd63e7a71fbaf6
jdreioe <dreioe@pm.me>
2026-02-18 13:45:39 +0100

    trying to fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 
 plugins {
     // Enable automatic Java toolchain provisioning from Foojay API
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "WingmateKMP"
